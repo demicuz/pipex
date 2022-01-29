35676af789efe2a2319e271d1e66870f47c37141
demicuz <pure.demicube@gmail.com>
2022-01-29 15:04:52 +0300

    save

--- src/pipeline.c
+++ src/pipeline.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include <pipex.h>
-#include <stdlib.h>
 
 // Returns an array of ints. Every pair corresponds to read/write fd.
 // The first one is fd_in, then pipes, the last one is fd_out.
